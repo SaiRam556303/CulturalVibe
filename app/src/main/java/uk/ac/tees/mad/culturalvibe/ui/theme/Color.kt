d2f1bc43b93c01ca999d28e216ffd5f761d3fcbf
Pantham Janaki Sai Ram <janakisairampantham0@gmail.com>
2025-10-30 12:00:00 +0530

    Added Adaptive app icon

--- app/src/main/java/uk/ac/tees/mad/culturalvibe/ui/theme/Color.kt
+++ app/src/main/java/uk/ac/tees/mad/culturalvibe/ui/theme/Color.kt
@@ -9,3 +9,28 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Primary Palette
+val PrimaryColor = Color(0xFF6A1B9A)        // Deep Purple
+val PrimaryVariant = Color(0xFF8E24AA)      // Royal Purple
+val OnPrimary = Color(0xFFFFFFFF)           // White
+
+// Secondary Palette
+val SecondaryColor = Color(0xFFFF7043)      // Vibrant Orange
+val SecondaryVariant = Color(0xFFF4511E)    // Deep Coral
+val OnSecondary = Color(0xFFFFFFFF)         // White
+
+// Accent Palette
+val AccentColor = Color(0xFFFBC02D)         // Golden Yellow
+val OnAccent = Color(0xFF000000)            // Black
+
+// Background & Surface
+val BackgroundColor = Color(0xFFFFF8E1)     // Light Cream
+val SurfaceColor = Color(0xFFFFFFFF)        // White
+val OnBackground = Color(0xFF212121)        // Dark Charcoal
+val OnSurface = Color(0xFF212121)           // Dark Charcoal
+
+// Support Colors
+val SuccessColor = Color(0xFF43A047)        // Green
+val ErrorColor = Color(0xFFE53935)          // Red
+val InfoColor = Color(0xFF1E88E5)           // Blue
